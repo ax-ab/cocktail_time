5d221e2462a9dc2eda5b6210a7415de024fcce5c
Axel Abildtrup <axabildtrup@hotmail.com>
2019-05-07 11:05:04 +0200

    Correct settings.py database_url

--- webdev/settings.py
+++ webdev/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
-import dj-database-url
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -98,7 +98,7 @@ DATABASES = {
 
 #Heroku specific database information
 db_from_env = dj_database_url.config()
-DATABASES[‘default’].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
