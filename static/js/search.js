95e478f662ee9a147aa3e9684cc119b1fa22a6ce
Axel Abildtrup <axabildtrup@hotmail.com>
2019-05-20 22:00:34 +0200

    Add loading animation when adding to favorites plus minor refactoring

--- static/js/search.js
+++ static/js/search.js
@@ -1,31 +1,35 @@
 // Page preloader. 1800 is exactly the time it takes for the full animation to complete
+var isSearchLandingPage = (window.location.pathname == '/search/' && window.location.search == '')
 $(window).on('load', function() {
 
     $("#loader").delay(1000).fadeOut("fast", function() {
 
         $("#cocktailSection, #search_results, #loadremaining_wrapper").fadeIn("slow");
         
+        console.log(isSearchLandingPage);
         //For '/search/' GET request as a demo of flipped card
-        if (window.location.pathname == '/search/' && window.location.search == '') {
+        if (isSearchLandingPage) {
             
             //Demo flip animation. Timeout is required as js doesnt know when the animation has ended
             function flip() {
                 $('#1_container').toggleClass('flipped');
             }
 
-            flip(
-                setTimeout(
-                    function(){ flip(
-                        setTimeout(
-                            function(){ $('#cocktailSection').fadeOut("slow", 
-                                function() {
-                                    $('#demo_search_hint').fadeIn("slow")
-                                }
-                            )}
-                        , 1000)
-                    )   }
-                , 1000)
-            );
+            setTimeout(
+                function() { flip(
+                    setTimeout(
+                        function(){ flip(
+                            setTimeout(
+                                function(){ $('#cocktailSection').fadeOut("slow", 
+                                    function() {
+                                        $('#demo_search_hint').fadeIn("slow")
+                                    }
+                                )}
+                            , 1000) //Fadeout
+                        )}
+                    , 1000) //Reverse flip
+                )}
+            , 1000); //Initial flip
 
         }
         // Scrolling to where we were last
@@ -55,7 +59,7 @@ $(window).on('load', function() {
 $(document).ready(function() {
 
     //Disables interactivity for the demosession
-    if (!(window.location.pathname == '/search/' && window.location.search == '')) {
+    if (!isSearchLandingPage) {
 
         // TOGGLE FLIP CARDS
         $(".outer-container-flip").on('click', function(){
@@ -85,15 +89,15 @@ $(document).ready(function() {
             console.log("name: " + $(this).attr('name'));
 
             if (this.className === empty) {
-                ajax_update_fav(drink_id, 'add')
+                ajax_update_fav(drink_id, 'add');
                 
             } else if (this.className === full) {
-                ajax_update_fav(drink_id, 'remove')
+                ajax_update_fav(drink_id, 'remove');
                 //this.className = empty;
             } else if (this.className === none) {
                 // $('#' + drink_id).popover('enable').popover('show');
                 //console.log('#' + drink_id + '_favorite')
-                alert('Please log in to add to favorites')
+                alert('Please log in to add to favorites');
             }
 
             //Below is needed so that the card is not flipped after pressing the favorite icon
@@ -115,30 +119,79 @@ $(document).ready(function() {
 
         var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
 
-        function ajax_update_fav(drink_id, update_type) 
-        {
-            $.ajax
-            ({
-                type: "POST",
-                url: "../update_favorites/",
-                data:{
-                        drink_id:drink_id,
-                        update_type:update_type                          
-                },
-                success: function ( data )
-                {
-                    // Reply from Django
-                    console.log('Django: ' + data) 
-                    // Updating the visual
-                    if (update_type === 'add') {
-                        $('#' + drink_id).removeClass().addClass('fas fa-heart card-badge');
-                    } else if (update_type === 'remove') {
-                        $('#' + drink_id).removeClass().addClass('far fa-heart card-badge');
-                    }            
+        function toggle_favloader(callback_type, drink_id, update_type) {
+                
+            if (callback_type == 'success') {
+
+                if (update_type == 'add') {
+
+                    setTimeout(function() {$('#' + drink_id + '_favorite_loader').fadeOut('slow')}, 300);
+                    setTimeout(function() {$('#' + drink_id).removeClass().addClass('fas fa-heart card-badge').fadeIn('slow')}, 800);
+
+                } else if (update_type == 'remove') {
+
+                    setTimeout(function() {$('#' + drink_id + '_favorite_loader').fadeOut('slow')}, 300);
+                    setTimeout(function() {$('#' + drink_id).removeClass().addClass('far fa-heart card-badge').fadeIn('slow')}, 800);
+
                 }
-            })
+
+            } else if (callback_type == 'error') {
+
+                //restoring the original class
+                if (update_type == 'add') {
+
+                    setTimeout(function() {$('#' + drink_id + '_favorite_loader').fadeOut('slow')}, 300);
+                    setTimeout(function() {$('#' + drink_id).removeClass().addClass('far fa-heart card-badge').fadeIn('slow')}, 800);
+
+                } else if (update_type == 'remove') {
+
+                    setTimeout(function() {$('#' + drink_id + '_favorite_loader').fadeOut('slow')}, 300);
+                    setTimeout(function() {$('#' + drink_id).removeClass().addClass('fas fa-heart card-badge').fadeIn('slow')}, 800);
+
+                }
+                
+                setTimeout(function() {alert('Connection issue. Please try again later')}, 1000);
+
+            } else {
+                //Clearing the badge icon and loading the anitmation
+                $('#' + drink_id).removeClass().addClass('card-badge');
+                $('#' + drink_id + '_favorite_loader').show();
+                $('#' + drink_id + '_favorite_loader').fadeIn('slow');
+                
+            }
+ 
         }
 
-}
+        function ajax_update_fav(drink_id, update_type) 
+        {
+            console.log('#' + drink_id);
+            toggle_favloader('',drink_id);
+
+                $.ajax
+                ({
+                    type: "POST",
+                    url: "../update_favorites/",
+                    data:{
+                            drink_id:drink_id,
+                            update_type:update_type                          
+                    },
+                    success: function ( data )
+                    {
+                        // Reply from Django
+                        console.log('Success. Server message: ' + data);
+                        // Updating the visual
+                        toggle_favloader('success', drink_id, update_type);
+                    },
+                    error: function ( data )
+                    {
+                        console.log('Error. Server message: ' + data);
+                        // Updating the visual
+                        toggle_favloader('error', drink_id, update_type);
+                    }
+                })
+
+        };
+
+};
 
 });
